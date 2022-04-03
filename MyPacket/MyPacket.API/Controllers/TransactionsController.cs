24bff6df1daf80813995a79c28df141598bf8dfc
DESKTOP-LUS5SM9\Bezi <stepienkamil97@gmail.com>
2022-04-03 23:25:12 +0200

    Przykładowe zastosowanie loggera

--- MyPacket/MyPacket.API/Controllers/TransactionsController.cs
+++ MyPacket/MyPacket.API/Controllers/TransactionsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyPacket.Application.Services;
 using MyPacket.Domain;
+using SerilogTimings;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,16 +11,19 @@ namespace MyPacket.API.Controllers
     [ApiController]
     public class TransactionsController : ControllerBase
     {
+        private readonly ILogger<TransactionsController> _logger;
         private readonly ITransactionService _transactionService;
 
-        public TransactionsController(ITransactionService transactionService)
+        public TransactionsController(ILogger<TransactionsController> logger,ITransactionService transactionService)
         {
+            _logger = logger;
             _transactionService = transactionService;
         }
 
         [HttpGet]
         public ActionResult<List<Transaction>> Get()
         {
+            _logger.LogInformation("Get all Transactions");
             var result = _transactionService.GetAllTransactions();
             return Ok(result);
         }
@@ -27,6 +31,7 @@ namespace MyPacket.API.Controllers
         [HttpPost]
         public ActionResult<Transaction> PostTransation(Transaction transaction)
         {
+            _logger.LogInformation("Create transation");
             var result = _transactionService.CreateTransaction(transaction);
             return Ok(result);
         }
