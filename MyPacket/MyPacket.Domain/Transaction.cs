dc2ba7ebf48e5eecbeb00a8a1340b3f25e4a1ee3
DESKTOP-LUS5SM9\Bezi <stepienkamil97@gmail.com>
2022-04-05 22:00:19 +0200

    Model bazodanowy tranzakcji

--- MyPacket/MyPacket.Domain/Transaction.cs
+++ MyPacket/MyPacket.Domain/Transaction.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MyPacket.Domain
 {
-    public class Transaction
+    public class Transaction : BaseEntity
     {
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(128)]
+        public string Name { get; set; } = string.Empty;
+        
+        [MaxLength(512)]
+        public string Description { get; set; } = string.Empty;
+        
+        [Required]
         public double Amount  { get; set; }
-        public DateTime Date { get; set; }
+
+        [Required]
+        public bool IsExpense { get; set; } = false;
     }
 }
