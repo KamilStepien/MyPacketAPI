333a669eb8fa803e7912a02cb23414b515ae5471
DESKTOP-LUS5SM9\Bezi <stepienkamil97@gmail.com>
2022-04-05 21:58:27 +0200

    Automatyczne wypełnianie daty utworzenia i modyfikacji

---
+++ MyPacket/MyPacket.Domain/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace MyPacket.Domain
+{
+    public class BaseEntity
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
--- MyPacket/MyPacket.Infrastructure/MyPacketDbContext.cs
+++ MyPacket/MyPacket.Infrastructure/MyPacketDbContext.cs
@@ -9,5 +9,24 @@ namespace MyPacket.Infrastructure
             : base(options) {}
 
         public DbSet<Transaction> Transactions { get; set; }
+
+
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker.Entries()
+                .Where(e => e.Entity is BaseEntity && (e.State == EntityState.Added|| e.State == EntityState.Modified));
+
+            foreach(var entityEntry in entries)
+            {
+                ((BaseEntity)entityEntry.Entity).UpdatedDate = DateTime.Now;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((BaseEntity)entityEntry.Entity).CreatedDate = DateTime.Now;
+                }
+            }
+
+            return base.SaveChanges();
+        }
     }
 }
