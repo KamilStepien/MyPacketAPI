64095730ed46d88584ea45ac8f20afae4f465c84
DESKTOP-LUS5SM9\Bezi <stepienkamil97@gmail.com>
2022-04-05 23:51:50 +0200

    Dodanie migracji

---
+++ MyPacket/MyPacket.API/Migrations/20220405200228_Add_transaction.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPacket.API.Migrations
+{
+    public partial class Add_transaction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Transactions",
+                newName: "UpdatedDate");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Transactions",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsExpense",
+                table: "Transactions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Transactions",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "IsExpense",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "Transactions",
+                newName: "Date");
+        }
+    }
+}
